62f3e3da8493ac0960c422976c04dd0d5437a87d
Andy Dunkel <a.dunkel@pilz.de>
2020-10-18 21:16:53 +0200

    WPF Projekt angelegt

---
+++ src/WebcamConfigurationTool/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using Telerik.Windows.Controls;
+using Telerik.Windows.MediaFoundation;
+
+namespace WebcamConfigurationTool
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+
+        public MainWindow()
+        {
+            StyleManager.ApplicationTheme = new VisualStudio2019Theme();
+            InitializeComponent();
+
+            var webcams = RadWebCam.GetVideoCaptureDevices();
+
+            foreach (var mediaFoundationDeviceInfo in webcams)
+            {
+                ComboWebcams.Items.Add(mediaFoundationDeviceInfo.FriendlyName);
+            }
+        }
+
+        private void ComboWebcams_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var index = ComboWebcams.SelectedIndex;
+            if (index != -1)
+            {
+                WebCam.Stop();
+                var videoDevices = RadWebCam.GetVideoCaptureDevices();
+                var videoFormats = RadWebCam.GetVideoFormats(videoDevices[index]);
+                WebCam.Initialize(videoDevices[index], videoFormats[0]);
+                WebCam.Start();
+                
+            }
+        }
+    }
+}
